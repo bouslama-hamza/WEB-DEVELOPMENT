0112bd2107a39da6ed3f1c1c1829043d428fd00e
bouslama-hamza <ham.bousa98@gmail.com>
2021-07-17 10:26:05 +0100

    Last Edit

--- Envirenement/app.py
+++ Envirenement/app.py
@@ -1,9 +1,6 @@
 from flask import Flask ,render_template
-from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-db = SQLAlchemy(app)
 app.config["SECRET_KEY"] = 'UNS654SD22W81WD2F1S4E3FF1SDF'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -21,4 +18,4 @@ def service():
     return render_template("services.html")
 
 if __name__ == '__main__':
-    app.run(debug=True , port= 5021)
+    app.run(debug=True , port= 8000)
